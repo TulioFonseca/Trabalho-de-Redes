05d02d6243cd4b3a3476dc6294bc345f144582f0
Caio <caiosgon3@gmail.com>
2019-04-07 18:41:29 -0300

    Verificacao de colisao atualizada

--- client-server/sender.rb
+++ client-server/sender.rb
@@ -11,14 +11,15 @@ File.open('Mensagem/mensagem.txt', 'rb') do |file|
   quadro = Quadro.new(ip_destino, mensagem)
 
   random = Random.new
-  socket.write(quadro.toString())
+  # socket.write(quadro.toString())
   loop{
     number = random.rand(3) 
     if number == 0
-      print("DEU RUIM")
+      print("DEU RUIM - Tentando novamente em 5 segundos\n")
       sleep 5
       socket.write(quadro.toString())
     else
+      socket.write(quadro.toString())
       break
     end
   }
