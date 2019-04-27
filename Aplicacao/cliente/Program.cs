3cd8a522cfb61a3ecdfecb1b248590ca4aec263a
Caio <caiosgon3@gmail.com>
2019-04-27 16:56:48 -0300

    Refatorando

---
+++ Aplicacao/cliente/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip_servidor = "192.168.0.15";
+            string porta_servidor = "1";
+            Console.WriteLine("Hello World!");
+            TcpClient cliente = new TcpClient(); 
+            client.Connect(ip_servidor, porta_servidor);
+            client.Close();
+        }
+    }
+}
